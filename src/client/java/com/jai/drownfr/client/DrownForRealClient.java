ec29eceacb0f09f826f3dde31db8862d82bf766e
jaisai1709-afk <jaisai1709@gmail.com>
2026-06-01 11:36:27 +0530

    Fabric template imported

---
+++ src/client/java/com/jai/drownfr/client/DrownForRealClient.java
@@ -0,0 +1,10 @@
+package com.jai.drownfr.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DrownForRealClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
